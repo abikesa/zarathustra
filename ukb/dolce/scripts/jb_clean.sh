4d8777c6add9044e8ac80ecd5fa502c2d336ff00
Abimereki Muzaale <apollo@Athena.local>
2025-06-08 22:27:50 -0400

    obfuscate

---
+++ ukb/dolce/scripts/jb_clean.sh
@@ -0,0 +1,9 @@
+mkdir -p _build/html/pdfs _build/html/figures _build/html/media _build/html/data
+cp -r figures/* _build/html/figures/
+cp -r pdfs/* _build/html/pdfs/
+cp -r media/* _build/html/media/
+cp -r act1/app/* _build/html/act1/app
+cp -r data/* _build/html/data
+
+
+
